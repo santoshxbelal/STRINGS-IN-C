1c652273e0d59e7271051296931d40c987abda3b
Santosh Belal <watashiwa.santosh.8@gmail.com>
2024-11-13 21:51:19 +0545

    Create Modifications of string

---
+++ Modifications of string.c
@@ -0,0 +1,10 @@
+
+// ACESS OF STRING THROUGH INDEX !! 
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    char greeting [] = "Hello World !!";
+    printf ("            %c",greeting[2]); // OUTPUT WILL BE l
+    return 0;
+}
