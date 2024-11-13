5ed841f83697ac8dabc543447983268145498e4a
Santosh Belal <watashiwa.santosh.8@gmail.com>
2024-11-13 21:49:46 +0545

    Create String example

---
+++ String example.c
@@ -0,0 +1,10 @@
+// SIMPLE EXAMPLE OF STRING 
+
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    char greeting [] = "Namaste Santosh !! ";
+    printf ("%s",greeting);
+    return 0;
+}
